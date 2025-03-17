ee61f58c7f53b435e18245f7f1b97aa95904b6c4
yang3kc <yang3kc@gmail.com>
2025-03-16 22:32:25 -0400

    🔄 refactor(cleaning): enhance domain normalization by removing non-news domains and duplicates

--- workflow/clean_robertson.py
+++ workflow/clean_robertson.py
@@ -2,7 +2,7 @@
 Purpose:
 Clean and standardize domain names from Ronald Robertson's list of domains.
 Processes raw domain data to extract and normalize domain names by converting
-to lowercase and removing non-news, government, and education domains.
+to lowercase, removing non-news domains, and removing duplicates.
 
 Inputs:
 - TSV file containing domain data with columns "domain" and "news_is_news"
@@ -16,12 +16,9 @@ python clean_robertson.py data/raw/robertson_domains.tsv data/processed/robertso
 
 import pandas as pd
 import sys
-import utils
 
 input_file = sys.argv[1]
 output_file = sys.argv[-1]
-
-domains_to_remove = set(["gov", "edu"])
 
 robertson_df = pd.read_csv(input_file, sep="\t", usecols=["domain", "news_is_news"])
 
@@ -36,16 +33,7 @@ print(
 
 robertson_df["domain"] = robertson_df["domain"].str.lower()
 
-robertson_df["suffix"] = robertson_df.domain.apply(utils.extract_suffix)
-robertson_df = robertson_df[~robertson_df["suffix"].isin(domains_to_remove)][["domain"]]
-print(f"{len(robertson_df)} rows after removing government and education domains.")
-
 robertson_df.drop_duplicates(inplace=True)
 print(f"{len(robertson_df)} rows after dropping duplicates.")
-
-robertson_df = robertson_df[
-    ~robertson_df["domain"].isin(utils.list_of_domains_to_remove)
-][["domain"]]
-print(f"{len(robertson_df)} rows after removing non-news domains.")
 
 robertson_df.to_csv(output_file, index=False)
