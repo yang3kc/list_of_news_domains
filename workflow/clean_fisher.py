7b4f19f79a4158c320700a052111c1010afd6c9e
yang3kc <yang3kc@gmail.com>
2025-03-16 23:03:20 -0400

    ✨ feat(cleaning): enhance data cleaning by integrating dataset identifier

--- workflow/clean_fisher.py
+++ workflow/clean_fisher.py
@@ -6,18 +6,22 @@ converting to lowercase, and removing duplicates.
 
 Inputs:
 - CSV file containing Fisher source data with "domain" column
+- String identifier for the dataset
+- Output file path
 
 Outputs:
-- CSV file containing cleaned domain names in a single "domain" column
+- CSV file containing cleaned domain names with "domain" and "dataset" columns
+  identifying the source dataset
 
 Example:
-python clean_fisher.py data/raw/fisher.csv data/processed/fisher_cleaned.csv
+python clean_fisher.py data/raw/fisher.csv fisher data/processed/fisher_cleaned.csv
 """
 
 import pandas as pd
 import sys
 
 input_file = sys.argv[1]
+dataset = sys.argv[2]
 output_file = sys.argv[-1]
 
 fisher_df = pd.read_csv(input_file, usecols=["domain"])
@@ -32,4 +36,5 @@ fisher_df["domain"] = fisher_df.domain.str.replace("/", "")
 fisher_df.drop_duplicates(inplace=True)
 print(f"{len(fisher_df)} rows after dropping duplicates.")
 
+fisher_df["dataset"] = dataset
 fisher_df.to_csv(output_file, index=False)
