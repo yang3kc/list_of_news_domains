889e26ef8cc9e62f370d16e3db0c5e5e98e2a01d
yang3kc <yang3kc@gmail.com>
2025-03-16 23:00:27 -0400

    ✨ feat(cleaning): enhance data cleaning by incorporating dataset identification

--- workflow/clean_robertson.py
+++ workflow/clean_robertson.py
@@ -6,18 +6,20 @@ to lowercase, removing non-news domains, and removing duplicates.
 
 Inputs:
 - TSV file containing domain data with columns "domain" and "news_is_news"
+- String identifier for the dataset
 
 Outputs:
-- CSV file containing cleaned domain names in a single "domain" column
+- CSV file containing cleaned domain names with "domain" and "dataset" columns
 
 Example:
-python clean_robertson.py data/raw/robertson_domains.tsv data/processed/robertson_cleaned.csv
+python clean_robertson.py data/raw/robertson_domains.tsv robertson data/processed/robertson_cleaned.csv
 """
 
 import pandas as pd
 import sys
 
 input_file = sys.argv[1]
+dataset = sys.argv[2]
 output_file = sys.argv[-1]
 
 robertson_df = pd.read_csv(input_file, sep="\t", usecols=["domain", "news_is_news"])
@@ -36,4 +38,5 @@ robertson_df["domain"] = robertson_df["domain"].str.lower()
 robertson_df.drop_duplicates(inplace=True)
 print(f"{len(robertson_df)} rows after dropping duplicates.")
 
+robertson_df["dataset"] = dataset
 robertson_df.to_csv(output_file, index=False)
